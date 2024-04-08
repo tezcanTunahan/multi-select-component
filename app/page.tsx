7932b287cdecc97419c1b4c2d82b0a55ededbcdc
tunahan <ttezcan.1999@gmail.com>
2024-04-08 19:08:09 +0300

    Refactor import paths and add Loading and Error components in page.tsx and MultiSelectSearch.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,26 +3,33 @@ import React, { useState } from 'react';
 import MultiSelectSearch, { SelectOption } from '@/components/multiSelectSearch/MultiSelectSearch';
 import Link from 'next/link';
 import { useFetchCharacters } from '@/hooks/useFetchCharacters';
+import Loading from '@/components/Loading';
+import Error from '@/components/Error';
 
 export default function Home() {
   const [search, setSearch] = useState('');
   const [value, setValue] = useState<SelectOption[] | undefined>([]);
   const { options, loading, error } = useFetchCharacters(search);
-  const myWebsiteUrl = process.env.NEXT_PUBLIC_CREATOR_WEBSITE_URL as string;
 
   return (
     <main className='flex flex-col items-center justify-center mt-24'>
       <p>
         Created by
-        <Link href={myWebsiteUrl} target='_blank' className='text-gray-500 hover:text-gray-950 font-semibold'>
+        <Link href={process.env.NEXT_PUBLIC_CREATOR_WEBSITE_URL || '#'} target='_blank' className='text-gray-500 hover:text-gray-950 font-semibold'>
           {' '}
           Tunahan tezcan
         </Link>
       </p>
       <div className='w-10/12 md:w-6/12'>
-        <MultiSelectSearch value={value} setValue={setValue} options={options} search={search} setSearch={setSearch} />
-        {loading && <p>Loading...</p>}
-        {error && <p>{error}</p>}
+        <MultiSelectSearch
+          value={value}
+          setValue={setValue}
+          options={options}
+          search={search}
+          setSearch={setSearch}
+          error={error}
+          loading={loading}
+        />
       </div>
     </main>
   );
--- components/multiSelectSearch/MultiSelectSearch.tsx
+++ components/multiSelectSearch/MultiSelectSearch.tsx
@@ -2,6 +2,8 @@
 import React, { useState } from 'react';
 import SelectList from './subComponents/SelectList';
 import SearchBar from './subComponents/SearchBar';
+import Loading from '../Loading';
+import Error from '../Error';
 
 export type SelectOption = {
   value: string;
@@ -16,9 +18,11 @@ type SelectProps = {
   setValue: React.Dispatch<React.SetStateAction<SelectOption[] | undefined>>;
   search: string;
   setSearch: React.Dispatch<React.SetStateAction<string>>;
+  error?: string;
+  loading?: boolean;
 };
 
-export default function MultiSelectSearch({ options, value, setValue, search, setSearch }: SelectProps) {
+export default function MultiSelectSearch({ options, value, setValue, search, setSearch, error, loading }: SelectProps) {
   // filtered options based on the search value
   const [filteredOptions, setFilteredOptions] = useState(options);
 
@@ -26,6 +30,8 @@ export default function MultiSelectSearch({ options, value, setValue, search, se
     <div className='w-full'>
       <SearchBar options={options} value={value} setValue={setValue} search={search} setSearch={setSearch} setFilteredOptions={setFilteredOptions} />
       <SelectList filteredOptions={filteredOptions} value={value} setValue={setValue} search={search} setSearch={setSearch} />
+      {loading && <Loading />}
+      {error && <Error />}
     </div>
   );
 }
