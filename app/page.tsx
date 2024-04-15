45c943e6780d88288f2b016be277b153783aaae5
tunahan <ttezcan.1999@gmail.com>
2024-04-15 19:16:39 +0300

    Update creator name and add additional information in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -10,13 +10,51 @@ export default function Home() {
 
   return (
     <main className='flex flex-col items-center justify-center mt-24'>
-      <p>
-        Created by
-        <Link href={process.env.NEXT_PUBLIC_CREATOR_WEBSITE_URL || '#'} target='_blank' className='text-gray-500 hover:text-gray-950 font-semibold'>
-          {' '}
-          Tunahan tezcan
-        </Link>
-      </p>
+      <div className='mb-10'>
+        <h1 className='text-4xl mb-10'>
+          Created by
+          <Link href={process.env.NEXT_PUBLIC_CREATOR_WEBSITE_URL || '#'} target='_blank' className='text-sky-500 hover:text-sky-700 font-semibold'>
+            {' '}
+            Tunahan tezcan
+          </Link>
+        </h1>
+        <p>
+          This is a simple example of a multi-select-input component. It uses the{' '}
+          <Link href='https://rickandmortyapi.com/' target='_blank' className='text-sky-500 hover:text-sky-700 font-semibold'>
+            Rick and Morty API
+          </Link>{' '}
+          to search for characters.
+        </p>
+
+        <p>
+          To see the source code, visit the
+          <Link
+            href='https://github.com/tezcanTunahan/multi-select-component'
+            target='_blank'
+            className='text-sky-500 hover:text-sky-700 font-semibold'>
+            {' '}
+            github page
+          </Link>
+        </p>
+        <p>
+          You can also check out the
+          <Link href='https://www.npmjs.com/package/multi-select-input' target='_blank' className='text-sky-500 hover:text-sky-700 font-semibold'>
+            {' '}
+            npm package
+          </Link>
+        </p>
+        <p>
+          and github page of the npm package
+          <Link
+            href='https://github.com/tezcanTunahan/multi-select-component-package'
+            target='_blank'
+            className='text-sky-500 hover:text-sky-700 font-semibold'>
+            {' '}
+            here
+          </Link>
+        </p>
+      </div>
+
       <div className='w-10/12 md:w-6/12'>
         <MultiSelectSearch
           value={value}
@@ -28,11 +66,7 @@ export default function Home() {
             handleSearch(e.target.value);
           }}
         />
-
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et error maxime itaque nulla animi excepturi nemo! Inventore nobis corporis
-          molestias a omnis rem dolores, quia corrupti consequuntur delectus accusantium at? lorem*55
-        </p>
+        <pre>{JSON.stringify(value, null, 2)}</pre>
       </div>
     </main>
   );
